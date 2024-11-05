7c969b68ab58acbab7c625a59a88e0f99de7b4e6
nanooka <102428722+nanooka@users.noreply.github.com>
2024-11-05 17:28:40 +0400

    homegape

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,5 +1,55 @@
+import { useState } from "react";
 import "./searchBar.scss";
+import { CiSearch } from "react-icons/ci";
+
+const types = ["buy", "rent"];
 
 export default function SearchBar() {
-  return <div className="searchBar">SearchBar</div>;
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={1000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={1000000}
+          placeholder="Max Price"
+        />
+        <button>
+          {/* <img src="/search.png" alt="" /> */}
+          <CiSearch size={24} color="white" />
+        </button>
+      </form>
+    </div>
+  );
 }
