80c066b1b6cbfeeb262a6093a6673c021a49010f
nanooka <102428722+nanooka@users.noreply.github.com>
2025-03-25 00:01:05 -0400

    API WORKS CORRECT!

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -8,8 +8,12 @@ export const SocketContextProvider = ({ children }) => {
   const { currentUser } = useContext(AuthContext);
   const [socket, setSocket] = useState(null);
 
+  // useEffect(() => {
+  //   setSocket(io("http://localhost:4000"));
+  // }, []);
+
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("https://real-estate-socket-v13t.onrender.com"));
   }, []);
 
   useEffect(() => {
